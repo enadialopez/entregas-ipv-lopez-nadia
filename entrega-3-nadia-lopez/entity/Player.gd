6015165f187ee90c8d8d449372bcb2c8751f5336
enadialopez <enadialopez@gmail.com>
2023-09-10 13:21:54 -0300

    subo trabajo 3 y 4

---
+++ entrega-3-nadia-lopez/entity/Player.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+export (float) var speed:float = 80
+
+func _process(delta):
+	
+#	var direction:int = 0
+#
+#	if Input.is_action_pressed("move_left"):
+#		direction = -1
+#	elif Input.is_action_pressed("move_right"):
+#		direction = 1
+
+	
+	var direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")) 
+	position.x += direction * speed * delta
